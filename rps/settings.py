6439c941e195e36fd9266948a2037cec6b25d169
Shahmir Riddo <78295593+Shahmir-Riddo@users.noreply.github.com>
2022-12-26 22:25:24 +0600

    Update settings.py

--- rps/settings.py
+++ rps/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["web-production-7b04.up.railway.app"]
 
-
+CSRF_TRUSTED_ORIGINS = ["web-production-7b04.up.railway.app"]
 # Application definition
 
 INSTALLED_APPS = [
