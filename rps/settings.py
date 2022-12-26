847d54537702801ce77d38fb31e267c4930323cb
Shahmir Riddo <78295593+Shahmir-Riddo@users.noreply.github.com>
2022-12-27 00:03:22 +0600

    Update settings.py

--- rps/settings.py
+++ rps/settings.py
@@ -117,13 +117,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATIC_ROOT = os.path.join(CONFIG_ROOT, '/static/')                       
+STATIC_URL = '/static/'                                                         
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = (                                                            
+    os.path.join(CONFIG_ROOT, 'static'), 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-MEDIA_ROOT= os.path.join(BASE_DIR, "media")
-
+MEDIA_ROOT= os.path.join(BASE_DIR, "/media/")
+   
 MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
